a23e3bb4c84a20affa5061f480ebfcca668ffcbe
Le Xuan Hoang <lxhoang96@gmail.com>
2020-02-23 00:17:19 +0700

    Fixed grouped bar charts Creat stacked bar charts

--- barchart.py
+++ barchart.py
@@ -1,14 +1,21 @@
 import matplotlib.pylab as plt
 import numpy as np
-
+import statistics
 list_colors = [(0.45, 0.58, 0.8), (0.88, 0.59, 0.3), (0.52, 0.73, 0.36), (0.83, 0.37, 0.38), (0.5, 0.52, 0.52), (0.56, 0.4, 0.65), (0.67, 0.41, 0.34), (0.8, 0.76, 0.06)]
+barWidth = 0.5
 
 
 class BarChart:
-    def __init__(self, x=None, objects=None):
+    def __init__(self, x=None, objects=None, title='', position=''):
         self.x = x
         self.objects = objects
-
+        self.title = title
+        self.position = position
+        self.basic_position = np.arange(len(self.objects))
+        self.data = []
+        for i in range(len(self.x)):
+            data = np.array(self.x[i])
+            self.data.append(data)
 
     def get_color(self, value=0):
         colors = []
@@ -17,11 +24,58 @@ class BarChart:
             colors.append(color)
         return colors
 
-    def basic_bar_chart(self, unit='', title=''):
-        y_value = np.arange(len(self.objects))
-        plt.bar(y_value, self.x, align='center', alpha=0.5, color=self.get_color(len(self.objects)))
-        plt.xticks(y_value, self.objects)
-        plt.ylabel(unit)
-        plt.title(title)
-        plt.autoscale(tight=True)
+    def basic_bar_chart(self, unit=''):
+        plt.title(self.title)
+        if self.position == 'vertical':
+            plt.bar(self.basic_position, self.data, align='center', color=self.get_color(len(self.objects)))
+            plt.xticks(self.basic_position, self.objects)
+            plt.ylabel(unit)
+        elif self.position == 'horizontal':
+            plt.barh(self.basic_position, self.data, align='center', color=self.get_color(len(self.objects)))
+            plt.yticks(self.basic_position, self.objects)
+            plt.xlabel(unit)
+        else:
+            print('Not supported')
 
+    def multi_bar(self, label_name=None, x_name=''):
+        list_position = [self.basic_position]
+        plt.title(self.title)
+        bar = []
+        for i in range(len(self.data)):
+            position = [x + barWidth*(i+1) for x in self.basic_position]
+            list_position.append(position)
+        if self.position == 'vertical':
+            for i in range(len(self.data)):
+                p = plt.bar(list_position[i], self.data[i], color=list_colors[i], width=barWidth, edgecolor='white', label=label_name[i])
+                bar.append(p)
+            plt.xticks(self.basic_position + barWidth*int(len(self.x)/2), self.objects)
+            plt.xlabel(x_name, fontweight='bold')
+        elif self.position == 'horizontal':
+            for i in range(len(self.data)):
+                p = plt.barh(list_position[i], self.data[i], color=list_colors[i], width=barWidth, edgecolor='white', label=label_name[i])
+                bar.append(p)
+            plt.yticks(self.basic_position + barWidth*int(len(self.data)/2), self.objects)
+            plt.ylabel(x_name, fontweight='bold')
+        plt.legend(bar, label_name)
+
+    def stacked_bar(self, bar_name=''):
+        plt.title(self.title)
+        bar = []
+        bottom = 0
+        if self.position == 'vertical':
+            p = plt.bar(x=self.basic_position, height=self.data[0], width=barWidth, color=list_colors[0], align='center', yerr=1)
+            bar.append(p)
+            plt.xticks(ticks=self.basic_position, lable=self.objects)
+            for i in range(1, len(self.data)):
+                bottom += self.data[i-1]
+                p1 = plt.bar(x=self.basic_position, height=self.data[i], width=barWidth, bottom=bottom, color=list_colors[i], align='center', yerr=1)
+                bar.append(p1)
+        elif self.position == 'horizontal':
+            p0 = plt.barh(y=self.basic_position, width=self.data[0], height=barWidth, color=list_colors[0], align='center', xerr=1)
+            bar.append(p0)
+            plt.yticks(ticks=self.basic_position, label=self.objects)
+            for i in range(1, len(self.data)):
+                bottom += self.data[i-1]
+                p2 = plt.barh(y=self.basic_position, width=self.data[i], height=barWidth, left=bottom, color=list_colors[i], align='center', xerr=1)
+                bar.append(p2)
+        plt.legend(bar, bar_name)
--- testbarcharts.py
+++ testbarcharts.py
@@ -1,24 +1,30 @@
 from barchart import BarChart
 import matplotlib.pylab as plt
-from multibarchart import MultiBarChart
 
 
 def run_test():
-    # a = BarChart([3, 5, 9, 2, 6, 15], {'a', 'b', 'c', 'd', 'e', 'f'})
-    # a.basic_bar_chart('Usage', 'Programming language usage')
+    """
+    Test class Barcharts: Basic bar chart, Grouped bar chart, Stacked bar chart.
+
+    :return: No return.
+    """
+    # Basic bar chart
+
+    a = BarChart([3, 5, 9, 2, 6, 15], ['python', 'js', 'java', 'C', 'C++', 'Css'], 'Programming language usage', 'vertical')
+    a.basic_bar_chart('Usage')
+    plt.show()
+    # a.basic_bar_chart('Usage', 'Programming language usage', 'horizontal')
     # plt.show()
-    # a = BarChart([3, 5, 9, 2, 6, 15], {'a', 'b', 'c', 'd', 'e', 'f'})
-    # b = BarChart([20, 19, 35, 27, 16, 15], {'1', '2', '3', '4', '5', '6'})
+
+    # Grouped bar chart
+    # list_x = [[3, 5, 9, 2, 6, 15], [20, 19, 35, 27, 16, 15], [23, 5, 13, 4, 20, 40]]
+    # c = BarChart(list_x, ['a', 'b', 'c', 'd', 'e', 'f'], 'test', 'vertical')
+    # c.multi_bar(['var1', 'var2', 'var3'], 'objects_test')
+    #
+    # # Stacked bar chart
     list_x = [[3, 5, 9, 2, 6, 15], [20, 19, 35, 27, 16, 15], [23, 5, 13, 4, 20, 40]]
-    # c = BarChart([3, 5, 9, 2, 6, 15], {'q', 'w', 'e', 'r', 't', 'y'})
-    c = MultiBarChart(list_x, {'a', 'b', 'c', 'd', 'e', 'f'}, ['var1', 'var2', 'var3'])
-    c.multi_bar('test')
-    # # c.basic_bar_chart()
-    # plt.legend()
-    # plt.show()
-    # a.multi_bar('var1')
-    # b.multi_bar('var2')
-    plt.legend()
+    c = BarChart(list_x, ['a', 'b', 'c', 'd', 'e', 'f'], 'test', 'horizontal')
+    c.stacked_bar(['var1', 'var2', 'var3'])
     plt.show()
 
 run_test()
