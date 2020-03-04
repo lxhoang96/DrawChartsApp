122f5e763f87688e996f587f7066a335a3bf8829
Le Xuan Hoang <lxhoang96@gmail.com>
2020-03-04 22:19:56 +0700

    Start working with UI(desktop App)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.properties import ListProperty
 from kivy.uix.button import Button
-
+from kivy.core.window import Window
 
 class DrawChartsApp(App):
     """
@@ -12,3 +12,18 @@ class DrawChartsApp(App):
     """
     def __init__(self, **kwargs):
         super(DrawChartsApp, self).__init__(**kwargs)
+
+    def build(self):
+        """
+
+        :return:
+        """
+        self.title = 'Draw chart App'
+        self.root = Builder.load_file('app.kv')
+        Window.clearcolor = (1, 1, 1, 1)
+        Window.size = (700, 1300)
+        return self.root
+
+
+myApp = DrawChartsApp()
+myApp.run()
