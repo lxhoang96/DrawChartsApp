0c69b7383837bd3c6e70f057ddab97c52e07a99a
Le Xuan Hoang <lxhoang96@gmail.com>
2020-03-07 09:11:03 +0700

    Lazy days

--- areacharts.py
+++ areacharts.py
@@ -3,11 +3,17 @@ import matplotlib.pylab as plt
 
 class AreaCharts:
 
-    def __init__(self, objects=None, values=None):
+    def __init__(self, objects=None, values=None, title='', x_name='', y_name=''):
         self.objects = objects
         self.values = values
+        self.title = title
+        self.x_name = x_name
+        self.y_name = y_name
 
     def basic_chart(self, labels=None, location=''):
         fig, ax = plt.subplots()
         ax.stackplot(self.objects, self.values, labels=labels)
         ax.legend(loc=location)
+        plt.title(self.title)
+        plt.xlabel(self.x_name)
+        plt.ylabel(self.y_name)
--- main.py
+++ main.py
@@ -1,29 +1,41 @@
-from linechart import LineChart
+# from linechart import LineChart
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.properties import ListProperty
 from kivy.uix.button import Button
 from kivy.core.window import Window
+Chart_list = ['Area chart', 'Bar chart', 'Box plot', 'Bubble chart', 'Histogram', 'Line chart', 'Pie chart', 'Radar chart', 'Scatter chart']
+
 
 class DrawChartsApp(App):
     """
 
     """
+    Chart_list = ListProperty()
+
     def __init__(self, **kwargs):
         super(DrawChartsApp, self).__init__(**kwargs)
+
+        self.list_chart = Chart_list
+        self.first_chart = self.list_chart[0]
 
     def build(self):
         """
 
         :return:
         """
+        # Window.clearcolor = (1, 1, 1, 1)
+        # Window.size = (700, 1300)
         self.title = 'Draw chart App'
         self.root = Builder.load_file('app.kv')
-        Window.clearcolor = (1, 1, 1, 1)
-        Window.size = (700, 1300)
+
         return self.root
 
+    # def draw_charts(self, key):
+        # if str(key) == self.list_chart[0]:
 
-myApp = DrawChartsApp()
-myApp.run()
+
+
+if __name__ == '__main__':
+    DrawChartsApp().run()
